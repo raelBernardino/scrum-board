7875f735a0a8c3d3d2648d89c8212070052f3862
Rael Bernardino <raelbernardino@Sherwinloves-Mac-Pro.local>
2018-09-19 04:32:48 -0700

    Create board renaming functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,53 @@
 import React, { Component } from "react";
-import ListContainer from "./ListContainer";
 export default class Main extends Component {
   constructor(props) {
     super(props);
+    this._onChange = this._onChange.bind(this);
+    this._onClick = this._onClick.bind(this);
+    this._onKeyDown = this._onKeyDown.bind(this);
     this.state = {
-    };
+      boardTitle: 'Untitled',
+      isChangingBoardTitle: false
+    }; 
   }
+
+  _onChange(e) {
+    this.setState({
+      boardTitle: e.target.value
+    })
+  }
+
+  _onClick(){
+    this.setState({
+      isChangingBoardTitle: !this.state.isChangingBoardTitle
+    })
+  }
+
+  _onKeyDown(e) {
+     if(e.keyCode === 13 || e.keyCode === 27) {
+       this.setState({
+         isChangingBoardTitle: !this.state.isChangingBoardTitle
+       })
+     }
+  } 
 
   render() {
     return (
-      <div className="Main"> 
-      
+      <div className="main"> 
+        { this.state.isChangingBoardTitle ?
+            <input 
+              type="text" 
+              className="board-title-input" 
+              onChange={this._onChange} 
+              placeholder={this.state.boardTitle}
+              onKeyDown={this._onKeyDown}
+              />
+          : <button 
+              className="board-title-btn"
+              onClick={this._onClick}>
+            {this.state.boardTitle}
+            </button>
+        }
       </div>
     );
   }
